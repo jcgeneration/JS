db6f906e2deb3aaed1040bbce262eb3f2d3d4f5c
jc <juan.valencia@idr.edu.mx>
2023-03-27 17:20:20 -0600

    Se resolvió el ejercicio #1 - Parte #3

--- JS-04 - Arrays and Loops/js/main.js
+++ JS-04 - Arrays and Loops/js/main.js
@@ -1,7 +1,12 @@
 let fila = ["Sofia", "David", "Juan"];
+console.log("Parte #1");
 console.log(fila);
 fila.push("Sara");
 fila.push("Augustin");
 fila.shift();
+console.log("Parte #2");
 console.log(fila);
-
+fila.splice(1,0, "Renata");
+fila.push("Elena");
+console.log("Parte #3");
+console.log(fila);
